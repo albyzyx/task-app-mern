0c142e110dcd5f28413da0303a47de01271178b4
tharun0120 <81463413+tharun0120@users.noreply.github.com>
2021-12-30 17:34:51 +0530

    fixes for production

--- server/src/index.js
+++ server/src/index.js
@@ -34,6 +34,12 @@ app.get("/api/ping", (req, res) => {
   res.send(new Date().getTime().toString());
 });
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log("server is up on port " + port);
 });
